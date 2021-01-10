fe44d85ea36d82006f026bda5152fc1e57a5d622
rael martins <raelpgm2@gmail.com>
2021-01-10 18:01:30 -0300

    Initial setup

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperio.dsdelive;
+package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsdeliveApplication {
+public class DsdeliverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsdeliveApplication.class, args);
+		SpringApplication.run(DsdeliverApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/devsuperior/dsdeliver/DsdeliverApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dsdeliver/DsdeliverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperio.dsdelive;
+package com.devsuperior.dsdeliver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsdeliveApplicationTests {
+class DsdeliverApplicationTests {
 
 	@Test
 	void contextLoads() {
